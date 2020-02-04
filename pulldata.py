ca8a62ea653896e5b856b62a0aaf90f4d0b407bb
Justin Buist <justinb@countrysidegreenhouse.com>
2020-02-03 19:25:58 -0500

    Match new firestore DB schema

--- pulldata.py
+++ pulldata.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-
+import json
 from google.cloud import firestore
 
 student_lookup = {}
@@ -10,10 +10,15 @@ for doc in students_ref.stream():
     student_lookup[doc.id] = doc.to_dict()['name']
 
 results_ref = db.collection('scoutresults')
+out_results = []
 for doc in results_ref.stream():
-    (year, match_id, team, student_key) = doc.id.split(':')
+    (year, compname, team, student_key, match_id) = doc.id.split(':')
     student_name = student_lookup[student_key]
     match_data = doc.to_dict()
-    print(f"Year {year} match {match_id} with {team} taken by {student_name}")
-    print("... crossed auto line: {}".format(match_data['auto_line']))
-    print("... auto bottom port: {}".format(match_data['auto_port_bottom']))
+    res = {'team_number': team,
+           'student_name': student_name,
+           'auto_line': match_data['auto_line'],
+           'auto_port_bottom': match_data['auto_port_bottom'],
+           }
+    out_results.append(res)
+print(json.dumps(out_results, indent=4, sort_keys=True))
